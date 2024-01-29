de09323b3cc73b502993504b8d492fee524cdd5c
Dirk W <dirk.woellhaf@gmail.com>
2024-01-29 19:11:24 +0100

    .

---
+++ Section-2/main.tf
@@ -0,0 +1,25 @@
+terraform {
+   required_providers {
+      thousandeyes = {
+      source = "thousandeyes/thousandeyes"
+      version = ">= 1.3.1"
+      }
+    } 
+}
+
+
+
+provider "thousandeyes" {
+    token = "insert-your-OAuth-bearer-token-here"      
+    account_group_id = "1712921"
+}
+
+resource "thousandeyes_http_server" "api_thousandeyes_http_test" { 
+    test_name = "ThousandEyes API Test - User <#>"
+    interval = 60
+    alerts_enabled = false
+    url = "https://api.thousandeyes.com/status.json"
+    agents {
+      agent_id = 61
+    }
+}
---
+++ Section-4/main.tf
@@ -0,0 +1,27 @@
+terraform {
+   required_providers {
+      thousandeyes = {
+      source = "thousandeyes/thousandeyes"
+      version = ">= 1.3.1"
+      }
+    } 
+}
+
+variable "te_oauth_token" {
+   type = string
+ }
+
+provider "thousandeyes" {
+    token = var.te_oauth_token      
+    account_group_id = "1712921"
+}
+
+resource "thousandeyes_http_server" "api_thousandeyes_http_test" { 
+    test_name = "ThousandEyes API Test - User <#>"
+    interval = 60
+    alerts_enabled = false
+    url = "https://api.thousandeyes.com/status.json"
+    agents {
+      agent_id = 61
+    }
+}
